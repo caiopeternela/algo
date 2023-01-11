7c4cf415a936acafe3a448e01625afb04fe07581
Caio Peternela <caiopeternela.dev@gmail.com>
2023-01-11 12:22:57 -0300

    add 2469

---
+++ leetcode/easy/python/2469.py
@@ -0,0 +1,11 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> list[float]:
+        to_kelvin = celsius + 273.15
+        to_fahrenheit = (celsius * 1.8) + 32
+        return [to_kelvin, to_fahrenheit]
+
+
+test = Solution()
+
+assert test.convertTemperature(36.50) == [309.65000,97.70000]
+assert test.convertTemperature(122.11) == [395.26000,251.79800]
