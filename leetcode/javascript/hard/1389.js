12528376e3ce12d29dd79c5a7af2468427102e17
Caio Peternela <caiopeternela.dev@gmail.com>
2023-01-13 23:23:44 -0300

    add hard ones

---
+++ leetcode/javascript/hard/1389.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} satisfaction
+ * @return {number}
+ */
+var maxSatisfaction = function(satisfaction) {
+  let result = 0
+  satisfaction.sort((x,y)=>x-y)
+  while(satisfaction.length > 0){
+      let sum = 0
+      for(let i in satisfaction){
+          sum = sum + (satisfaction[i] * (Number.parseInt(i)+1))
+      }
+      if (sum > result)
+          result = sum
+      satisfaction.shift()
+  }
+  return result
+}
---
+++ leetcode/javascript/hard/2113.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} plantTime
+ * @param {number[]} growTime
+ * @return {number}
+ */
+var earliestFullBloom = function(plantTime, growTime) {
+  let time = [], totalPlantTime = 0, totalBloomTime = 0
+
+  for (const i in plantTime) {
+      time.push([plantTime[i], growTime[i]])
+  }
+  time.sort((x, y) => y[1] - x[1])
+
+  for (const [plantTime, growTime] of time) {
+      totalPlantTime += plantTime
+      totalBloomTime = Math.max(totalBloomTime, totalPlantTime + growTime)
+  }
+  return totalBloomTime
+}
